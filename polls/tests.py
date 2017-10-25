22e509a6e3f9e37d6d6fbe67368ce2aa702fcbe5
Shane Moo <obries6@rpi.edu>
2017-10-24 21:41:26 -0400

    Got something finally. Still confused

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,35 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+class QuestionModelTests(TestCase):
+
+	def test_was_published_recently_with_future_question(self):
+		'''
+		ensures that questions published with a future date gets a false
+		from was_published_recently()
+		'''
+		time = timezone.now() + datetime.timedelta(days = 30)
+		future_question = Question(pub_date = time)
+		self.assertIs(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_question(self):
+		'''
+		ensures that questions published with a date that is older
+		than a day gets a false from was_published_recently()
+		'''
+		time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+		old_question = Question(pub_date = time)
+		self.assertIs(old_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_recent_question(self):
+		'''
+		ensures that questions published within the past 24 hours
+		get a true from was_published_recently()
+		'''
+		time = timezone.now() - datetime.timedelta(hours=23, minutes=59,  seconds=59)
+		recent_question = Question(pub_date = time)
+		self.assertIs(recent_question.was_published_recently(), True)
--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
 	#ex: /polls/
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'), ##assigning this url to the view index
 	#ex: /polls/5/
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='details'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'), ##assigning this url to the view detail
 	#ex: /polls/5/results/
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-	#ex: /polls//5/vote/
-	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'), ##assigning this url to the view results
+	#ex: /polls/5/vote/
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'), ##assigning this url to the view vote
+	#ex: /polls/ayylmao
+	url(r'^ayylmao/$', views.AyyLmao.as_view(), name='ayylmao'),
 ]
